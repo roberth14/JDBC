e44d8d57c66b137336868b0d87d44aaf23c93182
unknown <roberthdavidcamo@ufps.edu.co>
2023-11-19 20:29:15 -0500

    SE INICIA CON EL SERVLET

--- src/main/java/co/empresa/test/dao/UsuarioDAO.java
+++ src/main/java/co/empresa/test/dao/UsuarioDAO.java
@@ -1,7 +1,10 @@
 package co.empresa.test.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import co.empresa.test.modelo.Usuario;
 import co.empresa.test.util.Conexion;
@@ -26,9 +29,81 @@ public class UsuarioDAO {
 			preparedStatement.setString(1, usuario.getNombre());
 			preparedStatement.setString(2, usuario.getEmail());
 			preparedStatement.setString(3, usuario.getPais());
+			conexion.execute();
 		}catch (SQLException e) {
 			
 		}
+	}
+	
+	public void delete(int id) {
+		try {
+			PreparedStatement preparedStatement = conexion.setPreparedStatement(DELETE_USUARIO_SQL);
+			preparedStatement.setInt(1,id);
+
+			conexion.execute();
+		}catch (SQLException e) {
+			
+		}
+	}
+	
+	public void update (Usuario usuario) {
+		try {
+			PreparedStatement preparedStatement = conexion.setPreparedStatement(UPDATE_USUARIO_SQL);
+			preparedStatement.setString(1, usuario.getNombre());
+			preparedStatement.setString(2, usuario.getEmail());
+			preparedStatement.setString(3, usuario.getPais());
+			preparedStatement.setInt(4, usuario.getId());
+			conexion.execute();
+		}catch (SQLException e) {
+			
+		}
+	}
+	
+	public List<Usuario> selectAll(){
+		List<Usuario> usuarios = new ArrayList<>();
+		
+		try {
+		PreparedStatement preparedStatement = conexion.setPreparedStatement(SELECT_ALL_USUARIOS);
+
+			ResultSet rs = conexion.query();
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String nombre = rs.getString("nombre");
+				String email = rs.getString("email");
+				String pais = rs.getString("pais");
+				usuarios.add(new Usuario(id,nombre,email,pais));
+			}
+		}catch(SQLException e) {
+			
+		}
+		
+		
+		return usuarios;
+		
+	}
+	
+	public Usuario select(int id){
+		Usuario usuario = null;
+		
+		try {
+		PreparedStatement preparedStatement = conexion.setPreparedStatement(SELECT_USUARIO_BY_ID);
+        preparedStatement.setInt(1, id);
+		ResultSet rs = conexion.query();
+		
+			while(rs.next()) {
+	
+				String nombre = rs.getString("nombre");
+				String email = rs.getString("email");
+				String pais = rs.getString("pais");
+				usuario= new Usuario(id,nombre,email,pais);
+			}
+		}catch(SQLException e) {
+			
+		}
+		
+		
+		return usuario;
+		
 	}
 	
 }
--- src/main/java/co/empresa/test/modelo/Usuario.java
+++ src/main/java/co/empresa/test/modelo/Usuario.java
@@ -2,15 +2,28 @@ package co.empresa.test.modelo;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario implements Serializable {
 	
 	private Integer id;
 	private String nombre;
 	private String email;
 	private String pais;
+	
+	
+	public Usuario(int id, String nombre, String email, String pais) {
+		this.id=id;
+		this.nombre=nombre;
+		this.email=email;
+		this.pais=pais;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
